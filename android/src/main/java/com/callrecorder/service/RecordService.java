80cc2c7bbeaaa702f01dfe278b08b9791a1bd78f
hoang.nguyen <hoang.nguyen@platform.inc>
2024-08-02 10:26:27 +0700

    update service

--- android/src/main/java/com/callrecorder/service/RecordService.java
+++ android/src/main/java/com/callrecorder/service/RecordService.java
@@ -1,15 +1,28 @@
 package com.callrecorder.service;
 
+import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.annotation.SuppressLint;
+import android.graphics.PixelFormat;
 import android.media.MediaRecorder;
+import android.os.Handler;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.accessibility.AccessibilityEvent;
+import android.widget.FrameLayout;
 
 import java.io.IOException;
 
-public class RecordService {
+public class RecordService extends AccessibilityService {
 
     private MediaRecorder mediaRecorder;
     private String fileName;
     private String path;
+
+    public static final String LOG_TAG_S = "MyService:";
 
     public String getPath() {
         return path;
@@ -44,11 +57,11 @@ public class RecordService {
 
             Log.e("ReactNativePhoneCallRecorder", "started: " + file);
         } catch (IOException e) {
-            Log.e("ReactNativePhoneCallRecorder", "Microphone is already in use by another app. IOException: " + e.toString());
+            Log.e("ReactNativePhoneCallRecorder", "Microphone is already in use by another app. IOException: " + e);
         } catch (IllegalStateException e) {
-            Log.e("ReactNativePhoneCallRecorder", "Microphone is already in use by another app. IllegalStateException: " + e.toString());
+            Log.e("ReactNativePhoneCallRecorder", "Microphone is already in use by another app. IllegalStateException: " + e);
         } catch (Exception e) {
-            Log.e("ReactNativePhoneCallRecorder", "Something went wrong in start recording. Exception: " + e.toString());
+            Log.e("ReactNativePhoneCallRecorder", "Something went wrong in start recording. Exception: " + e);
         }
     }
 
@@ -59,7 +72,7 @@ public class RecordService {
         } catch (Exception e) {
             mediaRecorder.reset();
             mediaRecorder.release();
-            Log.e("stopping_failed", "Stop failed:" + e.toString());
+            Log.e("stopping_failed", "Stop failed:" + e);
         }
 
         mediaRecorder.reset();
@@ -67,4 +80,77 @@ public class RecordService {
         return path + "/" + fileName;
     }
 
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+
+  @SuppressLint("ClickableViewAccessibility")
+  @Override
+  protected void onServiceConnected() {
+    System.out.println("onServiceConnected");
+
+    //==============================Record Audio while  Call received===============//
+
+    WindowManager windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+    FrameLayout layout = new FrameLayout(this);
+
+    WindowManager.LayoutParams params = new WindowManager.LayoutParams(WindowManager.LayoutParams.MATCH_PARENT,
+      WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY,
+      WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE| WindowManager.LayoutParams.FLAG_FULLSCREEN |
+        WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE|
+        WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS|
+        WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
+      PixelFormat.TRANSLUCENT);
+    params.gravity = Gravity.TOP;
+
+    windowManager.addView(layout, params);
+    layout.setOnTouchListener(new View.OnTouchListener() {
+      @Override
+      public boolean onTouch(View v, MotionEvent event) {
+
+        //You can either get the information here or on onAccessibilityEvent
+
+
+
+        Log.e(LOG_TAG_S, "Window view touched........:");
+        Log.e(LOG_TAG_S, "Window view touched........:");
+        return true;
+      }
+    });
+
+    //==============To Record Audio wile Call received=================
+
+
+    AccessibilityServiceInfo info = new AccessibilityServiceInfo();
+    info.eventTypes = AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED;
+    info.eventTypes=AccessibilityEvent.TYPES_ALL_MASK;
+    info.feedbackType = AccessibilityServiceInfo.FEEDBACK_ALL_MASK;
+    info.notificationTimeout = 100;
+    info.packageNames = null;
+    setServiceInfo(info);
+
+
+    try {
+      startRecord();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+
+
+    new Handler().postDelayed(new Runnable() {
+      @Override
+      public void run() {
+        // This method will be executed once the timer is over
+        stopRecord();
+      }
+    }, 30000);
+
+  }
 }
